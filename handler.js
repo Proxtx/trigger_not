d59e08a22cb58e6adfef026766d2d2ef173a1e27
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-03-27 19:35:01 +0200

    Async

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 const triggerConfig = triggerGui.getElementsByClassName("triggerConfig")[0];
 
-getTriggerConfiguration(() => {
-  let config = triggerConfig.component.getTriggerConfiguration();
+getTriggerConfiguration(async () => {
+  let config = await triggerConfig.component.getTriggerConfiguration();
 
   return {
     text: "not " + config.text,
